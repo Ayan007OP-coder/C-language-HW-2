5f80c7fa1dd2388368ffcbb4e5017ecd06b5dd5c
Ayan007OP-coder <de.ayan05@gmail.com>
2024-10-31 15:31:57 +0530

    Update gross salary.c

--- gross salary.c
+++ gross salary.c
@@ -4,7 +4,7 @@ int b,sa,da,g;
 printf("enter your salary : ");
 scanf("%d",&b);
 if (b<10000){int sa=10*b/100,da=5*b/100,g=b+sa+da;
-("gross salary , basic salary , SA , DA\n ");
+printf("gross salary , basic salary , SA , DA\n ");
 printf("%d,%d,%d,%d\n",g,b,sa,da);}
 else if(b>10000&&b<20000){int sa=12*b/100,da=8*b/100,g=b+sa+da;
     printf("gross salary , basic salary , SA , DA\n");
